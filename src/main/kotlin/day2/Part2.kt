c08480eb94c6e9296780c0ba7d1e9ea28dc8a623
Thom Staal <thom.staal@rabobank.nl>
2024-01-04 16:21:10 +0100

    Day 2 part 2

--- src/main/kotlin/day2/Part2.kt
+++ src/main/kotlin/day2/Part2.kt
@@ -3,7 +3,39 @@ package day2
 import java.io.File
 
 fun main() {
-    File("src/main/kotlin/day2/input.in").forEachLine {line ->
-        println("hello")
+    var total = 0
+
+    File("src/main/kotlin/day2/input.in").forEachLine {game ->
+        val hands = mutableListOf<Map<String, Int>>()
+
+        game.split(": ")[1].split("; ").map{hand ->
+            val colorCount = mutableMapOf<String, Int>()
+
+            hand.split(", ").forEach(fun(cubeSet: String) {
+                val (count, color) = cubeSet.split(" ")
+
+                colorCount[color] = count.toInt()
+            })
+
+            hands.add(colorCount)
+
+        }
+
+
+        val highestHands = mutableMapOf<String, Int>()
+
+        hands.forEach{
+            it.forEach(fun(color, count) {
+                val currentHighestCount = highestHands[color]
+
+                if (currentHighestCount == null || count > currentHighestCount) {
+                    highestHands[color] = count
+                }
+            })
+        }
+
+        total += highestHands.values.reduce{total, current -> total * current}
     }
+
+    println(total)
 }
